c3f63d361f663df09880ddba5a60cb1b23be1969
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-05 14:51:05 +1000

    Add ClassicalMusic and RockMusic classes

---
+++ src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package com.alexkozyura.tutorial;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/com/alexkozyura/tutorial/RockMusic.java
@@ -0,0 +1,8 @@
+package com.alexkozyura.tutorial;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
