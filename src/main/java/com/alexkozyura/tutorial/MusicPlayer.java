30d1bd593d90726f98dfec9cd9d5d6193cba3edc
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-06 12:13:24 +1000

    Add di with some attributes of MusicPlayer class

--- src/main/java/com/alexkozyura/tutorial/MusicPlayer.java
+++ src/main/java/com/alexkozyura/tutorial/MusicPlayer.java
@@ -2,6 +2,24 @@ package com.alexkozyura.tutorial;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     // IoC
     public MusicPlayer(Music music) {
