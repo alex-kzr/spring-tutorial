eba1dd3c7129c0097950c24f5e41011167d9e41f
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 21:43:42 +1000

    Change to interface with only one implementation

--- src/main/java/com/alexkozyura/tutorial/RockMusic.java
+++ src/main/java/com/alexkozyura/tutorial/RockMusic.java
@@ -2,7 +2,6 @@ package com.alexkozyura.tutorial;
 
 import org.springframework.stereotype.Component;
 
-@Component("rockMusic")
 public class RockMusic implements Music{
     @Override
     public String getSong() {
