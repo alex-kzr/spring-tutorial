59fce0ec114f0754d5fb5d947c3bec17f2823852
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 23:05:54 +1000

    Add scope annotation example

--- src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
+++ src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
@@ -1,8 +1,10 @@
 package com.alexkozyura.tutorial;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class ClassicalMusic implements Music{
     @Override
     public String getSong() {
