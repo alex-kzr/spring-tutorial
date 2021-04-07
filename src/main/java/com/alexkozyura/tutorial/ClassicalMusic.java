6a4bafe7cb6c839b13e2881b58b51208eeb40420
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 23:10:23 +1000

    Add post constuct sample

--- src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
+++ src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
@@ -3,9 +3,16 @@ package com.alexkozyura.tutorial;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music{
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
