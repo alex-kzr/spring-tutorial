4c8638190593daa1b3f2a64e5ddadd76eb1ba188
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 23:44:17 +1000

    Remove autoscan and create beans in config

--- src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
+++ src/main/java/com/alexkozyura/tutorial/ClassicalMusic.java
@@ -1,12 +1,9 @@
 package com.alexkozyura.tutorial;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
 public class ClassicalMusic implements Music{
 
     @PostConstruct
