625823863e3a9be5a7628fd8c5816155797377d5
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 22:03:09 +1000

    Chose bean for DI using qualifier

--- src/main/java/com/alexkozyura/tutorial/MusicPlayer.java
+++ src/main/java/com/alexkozyura/tutorial/MusicPlayer.java
@@ -1,20 +1,16 @@
 package com.alexkozyura.tutorial;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    @Qualifier("rockMusic")
+    private Music music;
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 }
