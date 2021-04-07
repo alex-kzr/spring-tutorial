3deace3854812b98f1878476d940f52ac82dc4ec
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 14:13:58 +1000

    Add init and destroy methods for singleton bean

--- src/main/java/com/alexkozyura/tutorial/RockMusic.java
+++ src/main/java/com/alexkozyura/tutorial/RockMusic.java
@@ -1,6 +1,14 @@
 package com.alexkozyura.tutorial;
 
 public class RockMusic implements Music{
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Wind cries Mary";
