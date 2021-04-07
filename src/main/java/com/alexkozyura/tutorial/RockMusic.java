19e9847686a96cf45c7b22edb37b94866035953b
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-07 14:27:22 +1000

    Set bean default constructor as private. Add factory-method to bean.

--- src/main/java/com/alexkozyura/tutorial/RockMusic.java
+++ src/main/java/com/alexkozyura/tutorial/RockMusic.java
@@ -1,6 +1,12 @@
 package com.alexkozyura.tutorial;
 
 public class RockMusic implements Music{
+    private RockMusic() {}
+
+    public static RockMusic getRockMusic() {
+        return new RockMusic();
+    }
+
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
