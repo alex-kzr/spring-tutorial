97b99856a68ade40fe70beb589e608d60ef6a387
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-15 20:13:52 +1000

    Add filter for hidden http methods

--- src/main/java/com/alexkozyura/tutorial/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/alexkozyura/tutorial/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package com.alexkozyura.tutorial.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,16 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
