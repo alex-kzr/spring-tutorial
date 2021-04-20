7de4668452d05545520d2ed793184983b2e7bb8c
Alex.Kozyura <aleksey.kozyura@gmail.com>
2021-04-20 23:37:23 +1000

    Refactor UserDAO using JdbcTemplate

--- src/main/java/com/alexkozyura/tutorial/dao/UserDAO.java
+++ src/main/java/com/alexkozyura/tutorial/dao/UserDAO.java
@@ -1,138 +1,51 @@
 package com.alexkozyura.tutorial.dao;
 
 import com.alexkozyura.tutorial.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserDAO {
-    private static int PEOPLE_COUNT;
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/users_app";
-    private static final String USERNAME = "test_user";
-    private static final String PASSWORD = "1234";
+    private final JdbcTemplate jdbcTemplate;
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    @Autowired
+    public UserDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<User> index() {
-        List<User> users = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM app_user";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while (resultSet.next()) {
-                User user = new User();
-
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setAge(resultSet.getInt("age"));
-                user.setEmail(resultSet.getString("email"));
-
-                users.add(user);
-            }
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return users;
+        return jdbcTemplate.query("SELECT * FROM app_user", new UserMapper());
     }
 
     public User show(int id) {
-
-        User user = null;
-
-        try {
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM app_user WHERE id = ?");
-
-            preparedStatement.setInt(1, id);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-
-            user = new User();
-            user.setId(resultSet.getInt("id"));
-            user.setName(resultSet.getString("name"));
-            user.setAge(resultSet.getInt("age"));
-            user.setEmail(resultSet.getString("email"));
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return user;
+        return jdbcTemplate.query("SELECT * FROM app_user WHERE id=?",
+                new Object[]{id},
+                new UserMapper())
+                    .stream()
+                    .findAny()
+                    .orElse(null);
     }
 
     public void save(User user) {
-        try {
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO  app_user VALUES(1, ?, ?, ?)");
-
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setInt(2, user.getAge());
-            preparedStatement.setString(3, user.getEmail());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("INSERT INTO app_user VALUES (1, ?, ?, ?)",
+                user.getName(),
+                user.getAge(),
+                user.getEmail());
     }
 
     public void update(int id, User updatedUser) {
-        try {
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE  app_user SET name=?, age=?, email=? WHERE id=?");
-
-            preparedStatement.setString(1, updatedUser.getName());
-            preparedStatement.setInt(2, updatedUser.getAge());
-            preparedStatement.setString(3, updatedUser.getEmail());
-            preparedStatement.setInt(4, id);
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE app_user SET name=?, age=?, email=? WHERE id=?",
+                updatedUser.getName(),
+                updatedUser.getAge(),
+                updatedUser.getEmail(),
+                updatedUser.getId());
     }
 
     public void delete(int id) {
-
-        try {
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM app_user WHERE id=?");
-
-            preparedStatement.setInt(1, id);
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("DELETE FROM app_user WHERE id=?", id);
     }
 }
---
+++ src/main/java/com/alexkozyura/tutorial/dao/UserMapper.java
@@ -0,0 +1,22 @@
+package com.alexkozyura.tutorial.dao;
+
+import com.alexkozyura.tutorial.models.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        User user = new User();
+
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setAge(resultSet.getInt("age"));
+        user.setEmail(resultSet.getString("email"));
+
+        return user;
+    }
+}
